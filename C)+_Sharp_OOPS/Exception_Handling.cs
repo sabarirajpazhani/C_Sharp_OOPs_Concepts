8920193efc99c5aeadf964c3bea093a00097a294
sabarirajp <sabarirajpazhani@gmail.com>
2025-04-29 12:35:37 +0530

    Custom Exception

--- C)+_Sharp_OOPS/Exception_Handling.cs
+++ C)+_Sharp_OOPS/Exception_Handling.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace C_Sharp_OOPS
 {
@@ -47,11 +48,54 @@ namespace C_Sharp_OOPS
                 Console.WriteLine("StackTrace : "+e.StackTrace);
             }
         }
+
+        public static void throw_ManuallException()
+        {
+            Console.Write("Enter the Number 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the Number 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            throw new ArithmeticException("Number 2 is not be Zero");
+
+            Console.WriteLine(num1/num2);
+        }
+
+
         static void Main(string[] args)
         {
             //Logical_ExceptionHandling();
-            Try_Catch();
-            Console.ReadKey();
+            //Try_Catch();
+            //throw_ManuallException();
+
+
+
+
+            //------------------------------- Custom  Exception ----------------------------------
+            try
+            {
+                checkAge(17);
+            }
+            catch (UnderAgeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
+
+        public static void checkAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new UnderAgeException("You must be 18 or older to apply for a license.");
+            }
+
+            Console.WriteLine("You are Eligile");
+        }
+    }
+
+    public class UnderAgeException : Exception
+    {
+        public UnderAgeException(string message) : base(message){}
     }
 }
